36f6588051e0a255636c8069718101c741beb932
anne-an <anne.morke@gmail.com>
2021-07-06 11:27:36 +0300

    Check groups before deletion

--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -3,6 +3,8 @@ import random
 
 def test_delete_group(app):
     old_list = app.groups.get_group_list()
+    if len(old_list) == 1:
+        app.groups.add_new_group("my group")
     # group = random.choice(old_list)
     # print("GROUP   " + group)
     app.groups.delete_group("my group")
