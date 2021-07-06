74132dec7aba354c816594d78a037f73c1767618
anne_morke <anne.morke@gmail.com>
2021-07-06 13:43:39 +0300

    Load test data from excel

--- conftest.py
+++ conftest.py
@@ -1,3 +1,5 @@
+import os
+import openpyxl
 import pytest
 from fixture.application import Application
 
@@ -7,3 +9,18 @@ def app(request):
     fixture = Application("C:\\Users\\anne_\\Documents\\AddressBook\\AddressBook.exe")
     request.addfinalizer(fixture.destroy)
     return fixture
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("xl_"):
+            testdata = load_from_excel(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+def load_from_excel(file):
+    list = []
+    wb = openpyxl.load_workbook(os.path.join(os.path.dirname(os.path.abspath(__file__)), "generator/%s.xlsx" % file))
+    ws = wb.active
+    for row in range(1, ws.max_row + 1):
+        for cell in range(1, ws.max_column + 1):
+            list.append(ws.cell(row, cell))
+    return list
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,6 +1,6 @@
-def test_add_group(app):
+def test_add_group(app, xl_groups):
     old_list = app.groups.get_group_list()
-    app.groups.add_new_group("my group")
+    app.groups.add_new_group(xl_groups)
     new_list = app.groups.get_group_list()
-    old_list.append("my group")
+    old_list.append(xl_groups)
     assert sorted(old_list) == sorted(new_list)
